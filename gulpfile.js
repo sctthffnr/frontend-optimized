4a43540997179751cc0675c7d514455e114f0ae6
Scott Heffner <scott.thomas.heffner@gmail.com>
2016-04-09 11:46:11 -0400

    Added gulp task to resize large pizzeria.jpg file

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var cleanCSS = require('gulp-clean-css');
 var htmlMin = require('gulp-htmlmin');
+var imageResize = require('gulp-image-resize');
 
 gulp.task('minify-css', function() {
   gulp.src('src/css/*.css')
@@ -15,4 +16,13 @@ gulp.task('minify-html', function() {
   gulp.src('src/*.html')
     .pipe(htmlMin({collapseWhitespace: true}))
     .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('resize-image', function() {
+  gulp.src('src/views/images/pizzeria.jpg')
+    .pipe(imageResize({
+      width: 256,
+      height: 192
+    }))
+    .pipe(gulp.dest('dist/views/images/'));
 });
