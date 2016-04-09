7e3b93fb6ae34a36bf3fb9743fb72f5d93880291
Scott Heffner <scott.thomas.heffner@gmail.com>
2016-04-09 18:07:41 -0400

    Refactored changePizzaSize function to remove forced synchronous layout

--- src/views/js/main.js
+++ src/views/js/main.js
@@ -450,10 +450,13 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+    var pizzas = document.querySelectorAll(".randomPizzaContainer");
+    var pizza = pizzas[0];
+    var pizzaOffset = pizza.offsetWidth;
+    var dx = determineDx(pizza, size);
+    for (var i = 0; i < pizzas.length; i++) {
+      var newwidth = (pizzaOffset + dx) + 'px';
+      pizzas[i].style.width = newwidth;
     }
   }
 
