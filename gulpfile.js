d333f7bbcbfea652766b2fc00310d0f4f3bb501b
Scott Heffner <scott.thomas.heffner@gmail.com>
2016-04-09 10:47:54 -0400

    Created gulp task to minify html files

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var cleanCSS = require('gulp-clean-css');
+var htmlMin = require('gulp-htmlmin')
 
 gulp.task('minify-css', function() {
   gulp.src('src/css/*.css')
@@ -8,4 +9,10 @@ gulp.task('minify-css', function() {
   gulp.src('src/views/css/*.css')
     .pipe(cleanCSS({}))
     .pipe(gulp.dest('dist/views/css'));
+});
+
+gulp.task('minify-html', function() {
+  gulp.src('src/*.html')
+    .pipe(htmlMin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist/'));
 });
